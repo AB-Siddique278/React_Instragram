dc729259106d3140c38b47c1f63e66183a0437d5
AB-Siddique278 <absiddique1060@gmail.com>
2022-08-26 22:48:11 +0600

    create cardmenu

---
+++ src/Components/CardMenu.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../styles/cardMenu.scss"
+export default function CardMenu() {
+  return (
+    <div>CardMenu</div>
+    
+  )
+}
---
+++ src/Components/Comment.js
@@ -0,0 +1,10 @@
+import '../styles/comment.scss';
+import React from 'react'
+
+export default function Comment() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
