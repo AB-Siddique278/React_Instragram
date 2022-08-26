c566ea3d9e90d8ca270f5679aae25fa74b845f1a
AB-Siddique278 <absiddique1060@gmail.com>
2022-08-26 11:15:29 +0600

    create footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,21 @@ import React from 'react'
 import "../styles/footer.scss"
 export default function Footer() {
   return (
-    <div className='footer'>
+    <div className="footer">
+        <ul className="links">
+        <li>About</li>
+        <li>Help</li>
+        <li>Press</li>
+        <li>API</li>
+        <li>Jobs</li>
+        <li>Privacy</li>
+        <li>Terms</li>
+        <li>Locations</li>
+        <li>Top Accounts</li>
+        <li>Hashtags</li>
+        <li>Language</li>
+      </ul>
+      <div className="copyright">@ 2020 INSTAGRAM FROM META</div>
       
     </div>
   )
