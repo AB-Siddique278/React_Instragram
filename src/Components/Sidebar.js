ec93c459ae4bf8b0e7bd08b9a0380bb6f0291fd8
AB-Siddique278 <absiddique1060@gmail.com>
2022-08-27 20:49:46 +0600

    add readme file

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,29 +1,3 @@
-// import React from 'react'
-// import "../styles/sidebar.scss";
-// import Sticky from "react-sticky-el";
-// import Profile from "./Profile";
-
-// import image from "../images/profile.jpg";
-// import Suggestions from './Suggestions';
-// import Footer from './Footer';
-
-// export default function Sidebar() {
-//   return (
-//     <Sticky topOffset={-80}>
-//       <div className="sidebar">
-//         <Profile
-//           username="aleks.popovic"
-//           caption="Aleksandar Popović"
-//           urlText="Switch"
-//           iconSize="big"
-//           image={image}
-//         />
-//         <Suggestions />
-//         <Footer />
-//       </div>
-//     </Sticky>
-//   )
-// }
 
 import "../styles/sidebar.scss";
 import Sticky from "react-sticky-el";
